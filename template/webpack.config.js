50fc74e6ad5770054c987aacc141d6b582853ef6
cissiesee <496406128@qq.com>
2017-01-18 15:51:15 +0800

    min support

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -3,16 +3,41 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var CommonsChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin");
 var UglifyJsPlugin = require("webpack/lib/optimize/UglifyJsPlugin");
+var argv = process.argv;
+
+console.log(argv);
+
+var isDev = argv.indexOf('--dev') === -1 ? false : true;
+var afterfix = isDev ? '.js' : '.min.js';
+
+console.log('isDev:' + isDev);
+
+var plugins = [
+	new CommonsChunkPlugin('scripts/common' + afterfix, ['index', 'vender']),
+	new ExtractTextPlugin('styles/[name].css'),
+	new HtmlWebpackPlugin({
+		filename: __dirname + '/build/index.html',
+		template: __dirname + '/app/tpl/index.html',
+		chunks: ['scripts/common' + afterfix, 'index'] // 这个模板对应上面那个节点
+	})
+];
+
+if (!isDev) {
+	plugins.push(new UglifyJsPlugin({
+		minimize: true
+	}));
+}
+
 
 module.exports = {
 	entry: {
 		//'style': __dirname + '/app/styles/ui.less',
 		'index': __dirname + '/app/index.js',
-		'vender': ['react', 'react-immutable-render-mixin','react-dom', 'redux', 'react-redux']
+		'vender': ['react', 'react-immutable-render-mixin', 'react-dom', 'redux', 'react-redux']
 	},
 	output: {
 		path: __dirname + '/build',
-		filename: 'scripts/[name].js',
+		filename: 'scripts/[name]' + afterfix,
 		sourceMapFilename: 'scripts/[name].map'
 	},
 	//devtool: 'source-map',
@@ -24,31 +49,18 @@ module.exports = {
 			query: {
 				presets: ['react', 'es2015', 'stage-0']
 			}
-		}, /*{
-			test: /\.jsx?$/,
-			include: './app',
-			loaders: 'react-hot'
-		},*/ {
+		},{
 			test: /\.css$/,
 			//loader: ExtractTextPlugin.extract("style-loader","css-loader")
 			loader: 'style!css'
 		}, {
 			test: /\.less$/,
-			loader: ExtractTextPlugin.extract("style-loader","css-loader?sourceMap")
-			//loader: 'style!css!less'
+			loader: ExtractTextPlugin.extract("style-loader", "css-loader?sourceMap")
+				//loader: 'style!css!less'
 		}, {
 			test: /\.(png|jpg)$/,
 			loader: 'url?limit=25000'
 		}]
 	},
-	plugins:  [
-    	new CommonsChunkPlugin('scripts/common.js', ['index', 'vender']),
-    	new ExtractTextPlugin('styles/[name].css'),
-    	//new UglifyJsPlugin(),
-		new HtmlWebpackPlugin({
-            filename: __dirname + '/build/index.html',
-            template: __dirname + '/app/tpl/index.html',
-            chunks:['scripts/common.js', 'index']   // 这个模板对应上面那个节点
-        })
-	]
+	plugins: plugins
 };
